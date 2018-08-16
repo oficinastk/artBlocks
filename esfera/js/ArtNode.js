004f9936923521342241dbc8ef8d3321cb7d0d56
Oficinas TK <41126016+oficinastk@users.noreply.github.com>
2018-08-16 12:53:58 +0100

    Add files via upload

---
+++ esfera/js/ArtNode.js
@@ -0,0 +1,5 @@
+
+$.get( "http://art-blocks-endpoints.whatsthescore.webfactional.com/homestead/0x87d3073a473c858df1c9feaab685e4b62ae8c449", function( hashArtNode ) {
+  alert( "Hash: " + hashArtNode );
+    });
+var hash = hashArtNode;
